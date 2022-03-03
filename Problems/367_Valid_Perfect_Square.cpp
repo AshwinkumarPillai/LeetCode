b67491990081a150994be843626db02193bb87d6
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-03-03 18:08:27 +0530

    perfect square using binary search

--- Problems/367_Valid_Perfect_Square.cpp
+++ Problems/367_Valid_Perfect_Square.cpp
@@ -1,3 +1,4 @@
+// Loop on all number
 class Solution {
 public:
     bool isPerfectSquare(int num) {
@@ -15,6 +16,25 @@ public:
         
         if((i*i) == num){
             return true;
+        }
+        return false;
+    }
+};
+
+
+// Binary Search
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 0, high = num;
+        
+        long mid;
+        
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(mid * mid == num) return true;
+            if(mid * mid > num) high = mid - 1;
+            else low = mid+1;
         }
         return false;
     }
