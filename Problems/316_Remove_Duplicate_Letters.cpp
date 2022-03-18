c37b7cb8f474fa98fa8c9500d11c8dfacf79709b
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-03-18 23:15:31 +0530

    Replaced vector with map for better performance

--- Problems/316_Remove_Duplicate_Letters.cpp
+++ Problems/316_Remove_Duplicate_Letters.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        vector<int>lastIndex(26);
-        vector<bool>isCharInStack(26);
+        map<char, int>lastIndex;
+        map<char, bool>isCharInStack;
         int n = s.length();
         for(int i=0;i<n;i++){
-            lastIndex[s[i] - 'a'] = i;
+            lastIndex[s[i]] = i;
         }
         vector<char>st;
         for(int i=0;i<n;i++){
             if(st.size() == 0){
                 st.emplace_back(s[i]);
-                isCharInStack[s[i] - 'a'] = true;
+                isCharInStack[s[i]] = true;
             }
-            else if(!isCharInStack[s[i] - 'a']){
-                while(st.size() > 0 && i < lastIndex[st.back() - 'a'] && s[i] < st.back()){
-                    isCharInStack[st.back() - 'a'] = false;
+            else if(!isCharInStack[s[i]]){
+                while(st.size() > 0 && i < lastIndex[st.back()] && s[i] < st.back()){
+                    isCharInStack[st.back()] = false;
                     st.pop_back();
                 }
                 st.emplace_back(s[i]);
-                isCharInStack[s[i] - 'a'] = true;
+                isCharInStack[s[i]] = true;
             }
         }
         
