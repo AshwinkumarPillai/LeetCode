aeae57d29816f63c669a31443679f5cd9f283d50
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-03-30 08:45:17 +0530

    same logic updated code

--- Problems/74_Search_a_2D_matrix.cpp
+++ Problems/74_Search_a_2D_matrix.cpp
@@ -1,21 +1,16 @@
-class Solution
-{
+class Solution {
 public:
-    bool searchMatrix(vector<vector<int>> &matrix, int k)
-    {
-        int r = matrix.size();
-        if (r == 0)
-            return false;
-        int c = matrix[0].size();
-        int i = 0, j = c - 1;
-        while (i < r && j >= 0)
-        {
-            if (matrix[i][j] == k)
-                return true;
-            else if (matrix[i][j] > k)
-                j--;
-            else
-                i++;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0, j = n - 1;
+        
+        while(i<m && j >= 0){
+            int x = matrix[i][j];
+            if(x == target) return true;
+            if(target > x) i++;
+            else if(target < x) j--;
+            else return false;
         }
         return false;
     }
