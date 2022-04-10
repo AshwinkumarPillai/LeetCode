05c8246a2b6b8dac87c82b09b87730fec39d312e
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-04-10 12:54:14 +0530

    BaseBall game

---
+++ Problems/682_Baseball_Game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int>arr;
+        
+        for(string x: ops){
+            if(x == "C"){
+                arr.pop_back();
+            }else if(x == "D"){
+                arr.push_back(arr.back() * 2);
+            }else if(x == "+"){
+                int n = arr.size();
+                arr.push_back(arr[n-1] + arr[n-2]);
+            }else{
+                arr.push_back(stoi(x));
+            }
+        }
+        
+        return accumulate(arr.begin(), arr.end(), 0);
+    }
+};
