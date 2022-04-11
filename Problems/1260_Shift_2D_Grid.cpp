7a69c0f479c6746201def64d0128162ac2baa952
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-04-11 13:04:51 +0530

    updated code for shift 2d grid

--- Problems/1260_Shift_2D_Grid.cpp
+++ Problems/1260_Shift_2D_Grid.cpp
@@ -1,3 +1,25 @@
+// Updated solution
+class Solution {
+public:
+    
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int N = m*n;
+        k = k % N;
+        vector<vector<int>>ans(m, vector<int>(n));
+        for(int x  = 0; x < N; x++){
+            int i = x/n;
+            int j = x % n;
+            int ni = (i + (j + k) / n) % m;
+            int nj = (j+k) % n;
+            ans[ni][nj] = grid[i][j];
+        }
+        return ans;
+    }
+};
+
+// Initial solution
 class Solution {
 public:
     
