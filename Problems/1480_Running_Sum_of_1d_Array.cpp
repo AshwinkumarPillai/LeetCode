e56a9f9e555112c7e5feb53e044efe1b167c80b0
AshwinkumarPillai <ashwinfury1@gmail.com>
2022-06-07 11:16:02 +0530

    Running sum soln added

---
+++ Problems/1480_Running_Sum_of_1d_Array.cpp
@@ -0,0 +1,10 @@
+// In-place Running sum
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 1;i <n; i++) nums[i] += nums[i-1];
+        return nums;
+    }
+};
+
