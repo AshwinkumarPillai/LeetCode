7eb621255328e87b4c222d4ff94bb7d9abc8c1e7
Ashwin <ashwinfury1@gmail.com>
2024-10-24 16:35:36 -0400

    Doubly LinkedList Solution added

--- Problems/146_LRU_cache.cpp
+++ Problems/146_LRU_cache.cpp
@@ -1,3 +1,110 @@
+// MAP AND DOUBLY LINKED-LIST
+class LRUCache
+{
+public:
+    class Node
+    {
+    public:
+        int key;
+        int val;
+        Node *next;
+        Node *prev;
+
+        Node(int key = -1, int val = 0) : key(key), val(val), next(nullptr), prev(nullptr) {}
+
+        void changeVal(int val)
+        {
+            this->val = val;
+        }
+    };
+
+    map<int, Node *> m;
+    int capacity = 0;
+    int size = 0;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    LRUCache(int capacity)
+    {
+        head = new Node();
+        tail = new Node();
+        head->next = tail;
+        tail->prev = head;
+        this->capacity = capacity;
+    }
+
+    ~LRUCache()
+    {
+        Node *curr = head;
+        while (curr)
+        {
+            Node *nextNode = curr->next;
+            delete curr;
+            curr = nextNode;
+        }
+    }
+
+    void resetHead(Node *curr)
+    {
+        if (curr->prev)
+            curr->prev->next = curr->next;
+        if (curr->next)
+            curr->next->prev = curr->prev;
+
+        curr->next = head->next;
+        head->next->prev = curr;
+
+        head->next = curr;
+        curr->prev = head;
+    }
+
+    void removeLRU_Node()
+    {
+        Node *lastNode = tail->prev;
+        lastNode->prev->next = tail;
+        tail->prev = lastNode->prev;
+        m.erase(lastNode->key);
+        delete lastNode;
+        this->size--;
+    }
+
+    int get(int key)
+    {
+        if (m.find(key) == m.end())
+            return -1;
+        Node *curr = m[key];
+        resetHead(curr);
+
+        return m[key]->val;
+    }
+
+    void put(int key, int value)
+    {
+        if (m.find(key) != m.end())
+        {
+            m[key]->changeVal(value);
+            resetHead(m[key]);
+            return;
+        }
+        Node *curr = new Node(key, value);
+        m[key] = curr;
+        resetHead(curr);
+        this->size++;
+
+        if (size > capacity)
+            removeLRU_Node();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+// MY OLD CODE
 class LRUCache
 {
 public:
